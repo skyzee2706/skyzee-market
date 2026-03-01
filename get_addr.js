c3ed7147720467a7685e047bb7722b5cb81ec3a9
skyzee2706 <restu012415@gmail.com>
2026-03-02 04:37:00 +0800

    chore: add temp files to gitignore and remove from index

--- get_addr.js
+++
@@ -1,9 +0,0 @@
-const { ethers } = require('ethers');
-const fs = require('fs');
-require('dotenv').config();
-
-const pk = process.env.PRIVATE_KEY.startsWith('0x') ? process.env.PRIVATE_KEY : '0x' + process.env.PRIVATE_KEY;
-const wallet = new ethers.Wallet(pk);
-
-console.log("Saving true wallet address...");
-fs.writeFileSync('addr.txt', wallet.address);
